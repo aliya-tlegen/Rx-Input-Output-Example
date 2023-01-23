07f858906c488cf31e73cd06edd27dc87cc93a62
Aliya T <aliyatl@halykbank.kz>
2023-01-23 12:01:44 +0600

    Initial Commit

---
+++ RxExampleInputOutput/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxExampleInputOutput
+//
+//  Created by Алия on 23.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
